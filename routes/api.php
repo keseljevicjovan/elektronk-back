69648604a4d4425c935ea4d8cbae62af792e0bc6
keseljevicjovan <keseljevicjovan@proton.me>
2025-08-24 13:25:27 +0200

    feat(auth): add Spatie roles and permissions

--- routes/api.php
+++ routes/api.php
@@ -2,10 +2,16 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\Auth\AuthController;
+use Spatie\Permission\Middleware\RoleMiddleware;
 
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login',    [AuthController::class, 'login']);
 
 Route::middleware('auth:sanctum')->group(function () {
     Route::post('/logout', [AuthController::class, 'logout']);
+    Route::prefix('admin')->middleware(RoleMiddleware::class . ':admin')->group(function () {
+        Route::get('/whoami', function () {
+            return "root";
+        });
+    });
 });
